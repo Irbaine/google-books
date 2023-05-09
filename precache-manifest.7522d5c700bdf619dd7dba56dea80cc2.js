16cb60422ecc8b08e041f6700384da3df15fba75
Amine <irbaine@yahoo.com>
2023-05-09 16:55:49 +0000

    Updates

---
+++ precache-manifest.7522d5c700bdf619dd7dba56dea80cc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e4b11867bd09354a2de",
+    "url": "/google-books/static/css/main.cb82b457.chunk.css"
+  },
+  {
+    "revision": "5e4b11867bd09354a2de",
+    "url": "/google-books/static/js/main.818e55a2.chunk.js"
+  },
+  {
+    "revision": "d12899527c68d71d316c",
+    "url": "/google-books/static/js/runtime~main.d3fb5c4a.js"
+  },
+  {
+    "revision": "22f1d3f12e930879eb69",
+    "url": "/google-books/static/css/2.51bbabfb.chunk.css"
+  },
+  {
+    "revision": "22f1d3f12e930879eb69",
+    "url": "/google-books/static/js/2.253f5e7a.chunk.js"
+  },
+  {
+    "revision": "6885d93491a5f1f9f154",
+    "url": "/google-books/static/js/3.4f83f070.chunk.js"
+  },
+  {
+    "revision": "1bf6b607bb98bdc4eb2bf9177b240fce",
+    "url": "/google-books/index.html"
+  }
+];
